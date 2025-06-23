4e44481d3af8bf9c54c562c8b4a1c8fcce654b4f
dougllas.sousa <dougllas.sousa@seduc.ce.gov.br>
2025-06-23 08:26:57 -0300

    [pedidos] conexao kafka

---
+++ pedidos/src/main/java/io/github/cursodsousa/icompras/pedidos/service/PedidoService.java
@@ -0,0 +1,96 @@
+package io.github.cursodsousa.icompras.pedidos.service;
+
+import io.github.cursodsousa.icompras.pedidos.client.ServicoBancarioClient;
+import io.github.cursodsousa.icompras.pedidos.model.DadosPagamento;
+import io.github.cursodsousa.icompras.pedidos.model.Pedido;
+import io.github.cursodsousa.icompras.pedidos.model.enums.StatusPedido;
+import io.github.cursodsousa.icompras.pedidos.model.enums.TipoPagamento;
+import io.github.cursodsousa.icompras.pedidos.model.exception.ItemNaoEncontradoException;
+import io.github.cursodsousa.icompras.pedidos.repository.ItemPedidoRepository;
+import io.github.cursodsousa.icompras.pedidos.repository.PedidoRepository;
+import io.github.cursodsousa.icompras.pedidos.validator.PedidoValidator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PedidoService {
+
+    private final PedidoRepository repository;
+    private final ItemPedidoRepository itemPedidoRepository;
+    private final PedidoValidator validator;
+    private final ServicoBancarioClient servicoBancarioClient;
+
+    @Transactional
+    public Pedido criarPedido(Pedido pedido){
+        validator.validar(pedido);
+        realizarPersistencia(pedido);
+        enviarSolicitacaoPagamento(pedido);
+        return pedido;
+    }
+
+    private void enviarSolicitacaoPagamento(Pedido pedido) {
+        var chavePagamento = servicoBancarioClient.solicitarPagamento(pedido);
+        pedido.setChavePagamento(chavePagamento);
+    }
+
+    private void realizarPersistencia(Pedido pedido) {
+        repository.save(pedido);
+        itemPedidoRepository.saveAll(pedido.getItens());
+    }
+
+    public void atualizarStatusPagamento(
+            Long codigoPedido, String chavePagamento, boolean sucesso, String observacoes) {
+
+        var pedidoEncontrado = repository
+                .findByCodigoAndChavePagamento(codigoPedido, chavePagamento);
+
+        if(pedidoEncontrado.isEmpty()){
+            var msg = String.format("Pedido não encontrado para o código %d e chave pgmto %s",
+                    codigoPedido, chavePagamento);
+            log.error(msg);
+            return;
+        }
+
+        Pedido pedido = pedidoEncontrado.get();
+
+        if(sucesso){
+            pedido.setStatus(StatusPedido.PAGO);
+        } else {
+            pedido.setStatus(StatusPedido.ERRO_PAGAMENTO);
+            pedido.setObservacoes(observacoes);
+        }
+        repository.save(pedido);
+
+    }
+
+    @Transactional
+    public void adicionarNovoPagamento(
+            Long codigoPedido, String dadosCartao, TipoPagamento tipo){
+
+        var pedidoEncontrado = repository.findById(codigoPedido);
+
+        if(pedidoEncontrado.isEmpty()){
+            throw new ItemNaoEncontradoException("Pedido não encontrado para o código informado.");
+        }
+
+        var pedido = pedidoEncontrado.get();
+
+        DadosPagamento dadosPagamento = new DadosPagamento();
+        dadosPagamento.setTipoPagamento(tipo);
+        dadosPagamento.setDados(dadosCartao);
+
+        pedido.setDadosPagamento(dadosPagamento);
+        pedido.setStatus(StatusPedido.REALIZADO);
+        pedido.setObservacoes("Novo pagamento realizado, aguardando o processamento.");
+
+        String novaChavePagamento = servicoBancarioClient.solicitarPagamento(pedido);
+        pedido.setChavePagamento(novaChavePagamento);
+
+        repository.save(pedido);
+
+    }
+}
